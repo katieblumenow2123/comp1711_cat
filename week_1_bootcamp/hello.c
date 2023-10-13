d7423dc8c87e48e2b922e48cfa19196d182dfa3e
katieblumenow2123 <146842677+katieblumenow2123@users.noreply.github.com>
2023-10-13 13:52:19 +0000

    work

---
+++ week_1_bootcamp/hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    char mystring [] = "Hello";
+    int i=5;
+
+    while(i>=0)
+    {
+        printf("%c\n", mystring[i]);
+        i=i-1;
+        
+
+    }
+    return (0);
+}
---
+++ week_1_bootcamp/reverse.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int i=20;
+    while(i >=0)
+    {
+        if(i % 2 ==0)
+        {
+            printf("%d\n",i);
+
+        }
+        i--;
+    }
+}
